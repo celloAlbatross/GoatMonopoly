1775d593d846ee1424d1ea2ecc221f16caf937c1
FABIANA <Fabiana@FABIANAs-MacBook-Pro.local>
2016-12-22 10:37:06 +0700

    Set back-ground as BABY_BLUE

--- monopoly.py
+++ monopoly.py
@@ -6,7 +6,14 @@ SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
 
 class BetWindow(arcade.Window) :
-    def __init__(self) :
+    def __init__(self, width, height) :
         super().__init__(SCREEN_WIDTH, SCREEN_HEIGHT)
 
-        arcade.set_background_color(arcade.color.BABY_GRAY)
+        arcade.set_background_color(arcade.color.BABY_BLUE)
+
+    def on_draw(self) :
+        arcade.start_render()
+
+if __name__ == '__main__' :
+    window = BetWindow(SCREEN_WIDTH, SCREEN_HEIGHT)
+    arcade.run()
